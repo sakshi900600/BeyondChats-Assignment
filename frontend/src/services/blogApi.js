8efe98a49343d46a79bb4a35bf819b4dfbf83ac7
Sakshi <139939188+sakshi900600@users.noreply.github.com>
2025-12-31 13:09:32 +0530

    redeployed backend and removed the url from frontend and instead used env variable.

--- frontend/src/services/blogApi.js
+++ frontend/src/services/blogApi.js
@@ -1,14 +1,8 @@
 import axios from 'axios';
 
-// const API_BASE = 'http://localhost:5000/api/blogs';
-const API_BASE = 'https://beyond-chats-backend-inky.vercel.app/api/blogs';
+const API_BASE = import.meta.env.VITE_API_BASE_URL;
 
 export const blogApi = {
   getAll: () => axios.get(API_BASE),
   getById: (id) => axios.get(`${API_BASE}/${id}`),
-  create: (data) => axios.post(API_BASE, data),
-  update: (id, data) => axios.put(`${API_BASE}/${id}`, data),
-  delete: (id) => axios.delete(`${API_BASE}/${id}`),
-  rewrite: () => axios.post(`${API_BASE}/rewrite`),
-  scrape: () => axios.post(`${API_BASE}/scrape`),
 };
